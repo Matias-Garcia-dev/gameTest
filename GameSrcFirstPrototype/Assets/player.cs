e20a6368fd94a3221e2ff37800706b81e8e2048f
Matias <matiasenriquegarciadev@gmail.com>
2023-01-11 21:08:19 -0300

    commit

---
+++ GameSrcFirstPrototype/Assets/player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Vector2 input;
+    public float _speed;
+    public float inputx;
+    public float inputy;
+
+    private Rigidbody2D _rigi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigi = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputx = Input.GetAxis("Horizontal");
+        inputy = Input.GetAxis("Vertical");
+
+        _rigi.MovePosition(transform.position + (new Vector3(inputx, inputy, 0)));
+
+
+    }
+}
